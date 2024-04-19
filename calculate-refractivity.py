cb0bb05010e8a3fbdbf6840a9fe8716d85a647bd
sawyer-haase-lab <153561150+sawyer-haase-lab@users.noreply.github.com>
2024-04-19 11:03:36 -0700

    Update calculate-refractivity.py

--- calculate-refractivity.py
+++ calculate-refractivity.py
@@ -7,27 +7,10 @@ def calculate_refractivity(df):
     Rd = 287.058  # in J/kg/K
     Rv = 461.52   # in J/kg/K
     epsilon = Rd / Rv
-    
-    obs_spechum = df['obs_specificHumidity']
-    hofx_spechum = df['hofx_specificHumidity']
+    spechum = df['specificHumidity']
     p = df['pressure'] / 100  # hPA
-
-    obs_mixr = obs_spechum / (1 - obs_spechum)
-    hofx_mixr = hofx_spechum / (1 - hofx_spechum)
-
-    df['obs_watervaporPressure'] = p * (obs_mixr / (obs_mixr + epsilon))
-    df['hofx_watervaporPressure'] = p * (hofx_mixr / (hofx_mixr + epsilon))
-
-    htemp = 'hofx_airTemperature'
-    otemp = 'obs_airTemperature'
-    pressure = 'pressure'
-    hvapor = 'hofx_watervaporPressure'
-    ovapor = 'obs_watervaporPressure'
-
-    df['obs_refractivity'] = (77.6 * (df[pressure] / 100) / df[otemp]) + \
-                             ((3.73 * 10**5) * df[ovapor] / (df[otemp] ** 2))
-
-    df['hofx_refractivity'] = (77.6 * (df[pressure] / 100) / df[htemp]) + \
-                               ((3.73 * 10**5) * df[hvapor] / (df[htemp] ** 2))
-
+    mixr = spechum / (1 - spechum)
+    df['watervaporPressure'] = p * (mixr / (mixr + epsilon))
+    df['refractivity'] = (77.6 * (df['pressure'] / 100) / df['airTemperature']) + \
+                             ((3.73 * 10**5) * df['watervaporPressure'] / (df['airTemperature'] ** 2))
     return df
